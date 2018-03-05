21f98d3591e484158d96595f2f8d174d25b5cb40
kavya9sreedhar <kavya9sreedhar@gmail.com>
2018-03-04 20:03:00 -0800

    fixed syntax error

--- player.cpp
+++ player.cpp
@@ -42,6 +42,6 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
-	 start = 0;
+	int start = 0;
     return nullptr;
 }
